a05b5847c1a5413eae85e3fb8443ad46019621ff
Fernando de Lima Rodrigues <fernando_tecti@hotmail.com>
2026-08-21 11:20:06 -0300

    feat: icons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="icons/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="icons/favicon.svg" />
+        <link rel="shortcut icon" href="icons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="UniCalendar" />
+        <link rel="manifest" href="icons/site.webmanifest" />
 
         @fonts
 
